a88d3869806f00c9af0c99ff78c12983b1207012
Zhaksybek Satylgan <zhaksatylgan@Zhaksybeks-MacBook-Pro-2.local>
2024-01-31 10:15:16 -0600

    file upload

---
+++ src/test/java/day_3_xml_fileUpload_recap/FileUpload.java
@@ -0,0 +1,39 @@
+package day_3_xml_fileUpload_recap;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Test;
+import utilities.CashwiseAuthorization;
+
+import java.io.File;
+
+public class FileUpload {
+
+    @Test
+    public void fileUpload(){
+
+        String url = "https://backend.cashwise.us/api/myaccount/file/v2";
+        String token = CashwiseAuthorization.getToken();
+
+        String filePath = "dataToUpload.csv";
+
+        File file = new File(  filePath );
+
+        Response response = RestAssured.given()
+                .auth().oauth2( token )
+                .multiPart( "file", file, "multiple/form-data" )
+                .post( url);
+
+        System.out.println("My status code is: " + response.statusCode());
+
+        String fileName = response.jsonPath().getString("fileName");
+
+        System.out.println("My file name from response body: "+fileName);
+
+        response.prettyPrint();
+
+
+    }
+
+
+}
