fd1f81e742d60e755ec695c6cd53272db6555170
Zhaksybek Satylgan <zhaksatylgan@Zhaksybeks-MacBook-Pro-2.local>
2024-02-01 18:51:28 -0600

    Files changed

--- src/test/java/day_5_recap_pojo/BankAccountPractice.java
+++ src/test/java/day_5_recap_pojo/BankAccountPractice.java
@@ -1,6 +1,10 @@
 package day_5_recap_pojo;
 
 import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -29,7 +33,36 @@ public class BankAccountPractice {
         Serialization ==> Java request body => JSON object
          */
         RequestBody requestBody = new RequestBody(); // STEP-4  go inside RequestBody class and declare your variables
+        // STEP - 5 ==> set all RequestBody
+        requestBody.setType_of_pay("CASH");
+        requestBody.setBank_account_name(  faker.company().name()+ " bank"  );
+        requestBody.setDescription( faker.commerce().department()+ " company" );
+        requestBody.setBalance( faker.number().numberBetween(200, 15000)  );
 
+        /**
+         * {
+         *     "type_of_pay": "BANK",
+         *     "bank_account_name": " Featurea bank03",
+         *     "description": "Finacial company",
+         *     "balance": 150090
+         * }
+         */
+
+        // STEP - 6  ==> Hit api with RestAssured (POST)
+        Response response = RestAssured.given()
+                .auth().oauth2( getToken()  )
+                .contentType( ContentType.JSON )
+                .body( requestBody )
+                .post( url );
+
+        // STEP - 7  ==> Print out status code and make sure u have right status code (POST ==> 201)
+        System.out.println("My status code: "+   response.statusCode() );
+        // STEP - 8  ==> Assert your status code
+        Assert.assertEquals("Status code is NOT correct",201,   response.statusCode() );
+
+        // STEP - 9  ==> Print response body and make sure u created new BankAccount
+        System.out.println( "=====RESPONSE BODY===============================");
+        response.prettyPrint();
 
 
     }
