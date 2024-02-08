2693f259025ea96b82461c6163df09f9dc22b25f
Zhaksybek Satylgan <zhaksatylgan@Zhaksybeks-MacBook-Pro-2.local>
2024-02-07 19:12:30 -0600

    Files changed

--- src/test/java/steps/ProductController_stepDefinitions.java
+++ src/test/java/steps/ProductController_stepDefinitions.java
@@ -1,14 +1,19 @@
 package steps;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pojo.RequestBody;
 import utilities.APIRunner;
 
 public class ProductController_stepDefinitions {
+    RequestBody requestBody = new RequestBody();
+    Faker faker = new Faker();
 
     // declare your variable in class level
-    static String product_id ="";
+    static int product_id ;
     static String product_title="";
     static double product_price;
     static String product_description="";
@@ -28,12 +33,47 @@ public class ProductController_stepDefinitions {
 
     @Given("use RequestBody and create new product {string}")
     public void useRequestBodyAndCreateNewProduct(String path) {
-        System.out.println( path );
+      // Expected data
+        product_title = faker.commerce().productName();
+        product_price = faker.number().randomDouble(2,2,51);
+        product_description = faker.commerce().department();
+
+
+        requestBody.setProduct_title( product_title   );
+        requestBody.setProduct_price(product_price );
+        requestBody.setService_type_id(2);
+        requestBody.setCategory_id(805);
+        requestBody.setProduct_description( product_description );
+
+        APIRunner.runPOST(path, requestBody);
+
+        product_id = APIRunner.getCustomResponse().getProduct_id() ;
+
+        System.out.println( product_id );
+
+        apiPath = path + "/" + product_id;
+
+        System.out.println("=======New Product Created====================");
+        System.out.println("=======TEST PASSED!====================");
+
 
     }
 
     @When("get same product by id and validate data")
     public void getSameProductByIdAndValidateData() {
+        APIRunner.runGET(apiPath);
+        String actualProductTitle = APIRunner.getCustomResponse().getProduct_title();
+        double actualProductPrice = APIRunner.getCustomResponse().getProduct_price();
+        String actualProductDescription = APIRunner.getCustomResponse().getProduct_description();
+
+
+        System.out.println("=============TEST STARTED===================");
+        Assert.assertEquals( "Product Title is NOT correct", product_title, actualProductTitle  );
+        Assert.assertEquals( "Product Price is NOT correct", product_price+"", actualProductPrice  +"");
+        Assert.assertEquals( "Product Description is NOT correct", product_description, actualProductDescription  );
+
+        System.out.println("=============TEST PASSED===================");
+
 
 
     }
